1adbe6472e424fb03ecd0e4fc6a7dea9fe89a6aa
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-02-08 17:15:44 +0000

    refactor: env

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -18,7 +18,7 @@ export default class Server {
     };
 
     app.use(pino());
-    app.use(cors(corsOptions));
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
   }
