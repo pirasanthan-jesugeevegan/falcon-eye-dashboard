cea4322fb5b40c4c36da902871d9827c704ff513
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-02-09 09:30:14 +0000

    refactor: merge fix

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,18 +1,6 @@
-import express, { Application } from 'express';
-import dotenv from 'dotenv';
-import cors, { CorsOptions } from 'cors';
-import pino from 'express-pino-logger';
-import Routes from './routes';
+import server from './server';
 
-dotenv.config();
-const app = express();
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
-
-app.use(pino());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-new Routes(app);
-app.listen(PORT, 'localhost', function () {
-  console.log(`Server is running on port ${PORT}.`);
-});
+const port = process.env.PORT || 3001;
+server().listen(port, () =>
+  console.log(`App is listening on localhost:${port}`)
+);
