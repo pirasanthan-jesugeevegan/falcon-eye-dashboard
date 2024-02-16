9be2dcd3954fa0e80d1fd5c1d917c7d00efd802c
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2024-02-16 20:12:21 +0000

    home

--- packages/client/src/api/Common.ts
+++ packages/client/src/api/Common.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://sdsdsd-9p5b.onrender.com' : 'http://localhost:5001',
+    baseURL:
+        process.env.NODE_ENV === 'production'
+            ? 'https://sdsdsd-9p5b.onrender.com'
+            : 'http://localhost:5001',
     headers: {
         'Content-type': 'application/json',
     },
